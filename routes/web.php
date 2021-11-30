fddab0d644a712d95f5c34052e863cebf295be4b
Griswalde <agunggriswald158@gmail.com>
2021-11-30 12:25:22 +0700

    membuat navbar

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',function(){
+    return view('posts', [
+        "title" => "Posts"
+    ]);
+});
+
+
+Route::get('/Category',function(){
+    return view('Category');
 });
