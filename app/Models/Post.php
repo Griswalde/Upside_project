e6362a0adc07b1bec9e037ffe4eb79e2a186e939
Griswalde <agunggriswald158@gmail.com>
2021-12-02 15:29:06 +0700

    memperbaiki halaman posts yang error

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,7 +5,38 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Post
 {
-    use HasFactory;
+     private static $blog_posts = [
+        [
+        "title" => "judul Tulisan petama",
+        "slug" => "judul-tulisan-petama",
+        "author" => "mondo",
+        "body" => "judul pertama dan untuk keterakhir kalinya"
+    ],
+    [
+        "title" => "judul tulisan kedua",
+        "slug" => "judul-tulisan-kedua",
+        "author" => "luke",
+        "body" => "lorem saya ipsum dahar"
+    ]
+    ];
+
+    public static function all()
+    {
+        return self::$blog_posts;
+    }
+
+    public static function find($slug)
+    {
+        $posts = self::$blog_posts;
+        $post = [];
+        foreach($posts as $p) {
+            if($p["slug"] === $slug) {
+                $post = $p;
+            }
+        }
+        return $post;
+    }
+    
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,12 +2,16 @@
 
 @section('container')
 
-<h1 class="mb-5">New Post</h1>
 
 @foreach ($posts as $post)
-<article>
-    <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
-    <p>{{ $post->body }}</p>
+<article class="mb-5">
+    <h2>
+     <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
+    </h2>
+    <h5>By : {{ $post["author"] }}</h5>
+    <p>{{ $post["body"] }} </p>
 </article>
+
+@endforeach
 
 @endsection
