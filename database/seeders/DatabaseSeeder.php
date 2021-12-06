c9efaca41bd5fad0ac26598396ebe511050d6029
Griswalde <agunggriswald158@gmail.com>
2021-12-07 04:53:58 +0700

    menambahkan post dengat data dummy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,105 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+       
+     
+        User::factory(5)->create();
+
+
+
+        Category::create([
+            'name' => 'Movies',
+            'slug' => 'movie'
+        ]);
+ 
+         Category::create([
+             'name' =>'Series',
+             'slug' =>'series'
+         ]);
+
+         Category::create([
+            'name' =>'Drakor',
+            'slug' =>'drakor'
+        ]);
+
+        Category::create([
+            'name' =>'Anime',
+            'slug' =>'anime'
+        ]);
+
+        Post::factory(20)->create();
+
+
+
+        //  Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'I am the Bone of my Sword',
+        //     'body' => 'I am the Bone of my Sword
+        //     Steel is my Body and Fire is my Blood.
+        //     I have created over a Thousand Blades,
+        //     Unknown to Death,
+        //     Nor known to Life.
+        //     Have withstood Pain to create many Weapons
+        //     Yet those Hands will never hold Anything.
+        //     So, as I Pray',
+        //     'category_id' => 1,
+        //     'user_id' =>1
+
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'I am the Bone of my Sword',
+        //     'body' => 'I am the Bone of my Sword
+        //     Steel is my Body and Fire is my Blood.
+        //     I have created over a Thousand Blades,
+        //     Unknown to Death,
+        //     Nor known to Life.
+        //     Have withstood Pain to create many Weapons
+        //     Yet those Hands will never hold Anything.
+        //     So, as I Pray',
+        //     'category_id' => 1,
+        //     'user_id' =>1
+
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul ke tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'I am the Bone of my Sword',
+        //     'body' => 'I am the Bone of my Sword
+        //     Steel is my Body and Fire is my Blood.
+        //     I have created over a Thousand Blades,
+        //     Unknown to Death,
+        //     Nor known to Life.
+        //     Have withstood Pain to create many Weapons
+        //     Yet those Hands will never hold Anything.
+        //     So, as I Pray',
+        //     'category_id' => 2,
+        //     'user_id' =>1
+        // ]);
+
+        //     Post::create([
+        //         'title' => 'Judul ke Empat',
+        //         'slug' => 'judul-ke-empat',
+        //         'excerpt' => 'I am the Bone of my Sword',
+        //         'body' => 'I am the Bone of my Sword
+        //         Steel is my Body and Fire is my Blood.
+        //         I have created over a Thousand Blades,
+        //         Unknown to Death,
+        //         Nor known to Life.
+        //         Have withstood Pain to create many Weapons
+        //         Yet those Hands will never hold Anything.
+        //         So, as I Pray',
+        //         'category_id' => 2,
+        //         'user_id' =>2
+    
+           
+
+        // ]);
+
+
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,11 +4,16 @@
 
 
 @foreach ($posts as $post)
-<article class="mb-5">
+<article class="mb-5 border-bottom pb-4">
     <h2>
-     <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-    </h2>
+     <a href="/posts/{{ $post->slug }}"
+         class="text-decoration-none">{{ $post->title }}</a></h2>
+         
+    <p>By, <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>  </p>
+
     <p>{{ $post->excerpt }} </p>
+
+    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More...</a>
 </article>
 
 @endforeach
