86fc138eb746ace3337a0b39964ea6603ebfd8b2
Griswalde <agunggriswald158@gmail.com>
2021-12-08 03:22:21 +0700

    memperbaiki ui pada halaman categories

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -1,16 +1,57 @@
 @extends('layouts.main')
 
 @section('container')
-<h1 class="mb-5">Post Categories</h1>
+    <h1 class="mb-5">Post Categories</h1>    
 
-@foreach ($categories as $category)
-<ul>
-    <li>
+   
+        
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-md-4 mb-3">
+                <a href="/posts?category={{ $categories[0]->slug }}">
+                <div class="card bg-dark text-white">
+                    <img src="https://source.unsplash.com/500x500?" class="card-img" alt="{{ $categories[0]->name }}">
+                    <div class="card-img-overlay d-flex align-items-center p-0">
+                      <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0, 7)">{{ $categories[0]->name }}</h5>
+                    </div>
+                  </div>
+                </a>
+            </div>
+            <div class="col-md-4 mb-3">
+                <a href="/posts?category={{ $categories[1]->slug }}">
+                <div class="card bg-dark text-white">
+                    <img src="https://source.unsplash.com/500x500?" class="card-img" alt="{{ $categories[1]->name }}">
+                    <div class="card-img-overlay d-flex align-items-center p-0">
+                      <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0, 7)">{{ $categories[1]->name }}</h5>
+                    </div>
+                  </div>
+                </a>
+            </div>
+            <div class="col-md-4 mb-3">
+                <a href="/posts?category={{ $categories[2]->slug }}">
+                <div class="card bg-dark text-white">
+                    <img src="https://source.unsplash.com/500x500?" class="card-img" alt="{{ $categories[2]->name }}">
+                    <div class="card-img-overlay d-flex align-items-center p-0">
+                      <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0, 7)">{{ $categories[2]->name }}</h5>
+                    </div>
+                  </div>
+                </a>
+            </div>
+            <div class="col-md-4 mb-3">
+                <a href="/posts?category={{ $categories[3]->slug }}">
+                <div class="card bg-dark text-white">
+                    <img src="https://source.unsplash.com/500x500?" class="card-img" alt="{{ $categories[3]->name }}">
+                    <div class="card-img-overlay d-flex align-items-center p-0">
+                      <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0, 7)">{{ $categories[3]->name }}</h5>
+                    </div>
+                  </div>
+                </a>
+            </div>
+    
+        </div>
+    </div>
 
-    <h2><a href="/categories/{{ $category->slug }}">{{ $category->name }}</a></h2>
-    </li>
-</ul>
-
-@endforeach
+    
 
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,115 @@
 @extends('layouts.main')
 
 @section('container')
+<h1 class="mb-5">{{ $title }}</h1>
+
+@if($posts->count())
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+
+    {{-- image 1 --}}
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="http://source.unsplash.com/1200x400/?nature,water" class="d-block w-100" alt="{{ $posts[0]->category->name }}">
+        <div class="carousel-caption d-none d-md-block">
+            <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"
+                class="text-decoration-none text-info">{{ $posts[0]->title }} </a></h3>
+            <p>
+                <small class="text-muted">
+                By, <a href="/authors/{{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}
+                </a>{{ $posts[0]->created_at->diffForHumans() }}
+            </small>
+        </p>
+        </div>
+      </div>
+
+      {{-- image 2 --}}
+      <div class="carousel-item">
+        <img src="http://source.unsplash.com/1200x400/?nature,water" class="d-block w-100" alt="{{ $posts[1]->category->name }}">
+        <div class="carousel-caption d-none d-md-block">
+            <h3 class="card-title"><a href="/posts/{{ $posts[1]->slug }}"
+                class="text-decoration-none text-info">{{ $posts[1]->title }} </a></h3>
+            <p>
+                <small class="text-muted">
+                By, <a href="/authors/{{ $posts[1]->author->username }}">{{ $posts[1]->author->name }}
+                </a>{{ $posts[1]->created_at->diffForHumans() }}
+            </small>
+        </p>
+        </div>
+      </div>
+
+      {{-- image 3 --}}
+      <div class="carousel-item">
+        <img src="http://source.unsplash.com/1200x400/?nature,water" class="d-block w-100" alt="{{ $posts[2]->category->name }}">
+        <div class="carousel-caption d-none d-md-block">
+            <h3 class="card-title"><a href="/posts/{{ $posts[2]->slug }}"
+                class="text-decoration-none text-info">{{ $posts[2]->title }} </a></h3>
+            <p>
+                <small class="text-muted">
+                By, <a href="/authors/{{ $posts[2]->author->username }}">{{ $posts[2]->author->name }}
+                </a>{{ $posts[2]->created_at->diffForHumans() }}
+            </small>
+        </p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <br>
+{{-- <div class="card mb-3">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <h1 class="card-title">{{ $posts[0]->title }}</h1>
+        <p>
+            <small class="text-muted">
+            By, <a href="/authors/{{ $posts[0]->author->username }}">{{ $posts[0]->author->name }}
+            </a>{{ $posts[0]->created_at->diffForHumans() }}
+        </small>
+    </p>
+    </div>
+  </div> --}}
+@else
+<p class="text-center fs-4"> No Post Found. </p>
+@endif
 
 
-@foreach ($posts as $post)
-<article class="mb-5 border-bottom pb-4">
-    <h2>
-     <a href="/posts/{{ $post->slug }}"
-         class="text-decoration-none">{{ $post->title }}</a></h2>
-         
-    <p>By, <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a>  </p>
 
-    <p>{{ $post->excerpt }} </p>
+<div class="container">
+    <div class="row">
+        @foreach ($posts->skip(3) as $post)
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="position-absolute px-3 py-2 text-white" style="background-color:rgb(168, 168, 76)">
+                    <a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none">
+                    {{ $post->category->name }}</a></div>
+                <img src="http://source.unsplash.com/500x400?nature,water" class="card-img-top" alt="{{ $post->category->name }}">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $post->title }}</h5>
+                  <p>
+                    <small class="text-muted">
+                    By, <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}
+                    </a> {{ $post->created_at->diffForHumans() }}
+                </small>
+            </p>
+                  <p class="card-text">{{ $post->excerpt }}</p>
+                  <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read More</a>
+                </div>
+              </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 
-    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read More...</a>
-</article>
-
-@endforeach
 
 @endsection
