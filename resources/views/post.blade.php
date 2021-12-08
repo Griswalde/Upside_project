d4e1a7be333115bb7bc5bc3cf1b634bd909c9ab7
Griswalde <agunggriswald158@gmail.com>
2021-12-08 20:28:29 +0700

    memperbaiki tampilan ke author

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
      <div class="row justify-content-center mb-5">
           <div class="col-md-8">
                <h1 class="mb-3">{{ $post->title }}</h1>
-               <p>By. <a href="/authors/{{ $post->author->username }}"
+               <p>By. <a href="/posts?author={{ $post->author->username }}"
                     class="text-decoration-none">{{ $post->author->name }}</a>
                    </p>
 
